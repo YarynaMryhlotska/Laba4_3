42b8b2c1c263a91607f4b2b0ff3027a0c23f4e8a
YarynaMryhlotska <89984528+YarynaMryhlotska@users.noreply.github.com>
2021-10-06 13:57:56 +0300

    2

---
+++ Laba__4.3/Laba___4.3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x, xp, xk, dx, a, b, c, F;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+
+
+	cout << fixed;
+	cout << "----------------------" << endl;
+	cout << "|" << setw(7) << "x" << " |"
+		<< setw(10) << "F" << " |" << endl;
+	cout << "----------------------" << endl;
+
+	x = xp;
+	while (x <= xk)
+	{
+		if (c < 0 && b != 0)
+			F = (a * x * x) + (b * b * x);
+		else
+			if (c > 0 && b == 0)
+				F = (x + a) / (x + c);
+			else
+				F = (x / c);
+
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(6) << F
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "----------------------" << endl;
+	return 0;
+}
